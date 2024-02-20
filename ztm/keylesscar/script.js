c5e9c573285fdcb00fccd653a5dd5beb611b00ba
panhia-thao <pthao42@my.scccd.edu>
2024-02-20 09:17:44 -0800

    created files and css for keyless car file

---
+++ ztm/keylesscar/script.js
@@ -0,0 +1,10 @@
+let driver; 
+
+
+if (driver === 18) {
+    return "Congratulations on your first year of driving. Enjoy the ride!";
+} else if (driver < 18) {
+    return  "Powering On. Enjoy the ride!"
+} else if (driver > 18) {
+    return "Sorry, you are too young to drive this car. Powering off";
+}
